6c1db5666eab70070897bc2af1cd5706166dc0bb
egoitzaulestia <egoitzaulestia@gmail.com>
2025-04-26 19:48:50 +0200

    Continents

--- index.js
+++ index.js
@@ -31,11 +31,39 @@ const getCountriesData = async () => {
     }
 
     const countries = await response.json();
+    console.log(countries)
 
     // Sort countries by their common name
     countries.sort((a, b) => a.name.common.localeCompare(b.name.common));
 
+    // Empty array to push an array with all (repeated) continents
+    let allContinents = [];
+
+    // Go through all countries
     countries.forEach(country => {
+      country.continents.forEach(continent => {
+        allContinents.push(continent);
+      })
+    })
+
+    // Get unique continents
+    let uniqueContinents = [];
+    allContinents.forEach(continent => {
+      if (!uniqueContinents.includes(continent)) {
+        uniqueContinents.push(continent);
+      }
+    });
+    console.log(uniqueContinents)
+    
+    // Pro version of getting unique continents
+    // const continents = [...new Set(countries.flatMap(country => country.continents))];
+    // console.log(continents);
+
+    
+    countries.forEach(country => {
+      // if (loadCountriesByContinent) {
+        // countriesNode.innerHTML += loadCountriesByContinent(country)
+      // }
       countriesNode.innerHTML += cardTemplate(country);
     })
   } catch (error) {
@@ -49,3 +77,148 @@ getCountriesData();
 
 //TODO: una vez terminado el fetch pasarlo a axios
 
+
+
+
+
+// const loadGenres = async () => {
+//   try {
+//     // First get all genre IDs
+//     const response = await axios.get(`/genre/movie/list`, {
+//       params: {
+//         language: 'en-US'
+//       }
+//     })
+
+//     const genres = response.data.genres;
+//     console.log('Genres loaded:', genres);
+
+//     genres.forEach(genre => {
+//       const option = document.createElement('option');
+//       option.value = genre.id;
+//       option.textContent = genre.name;
+//       genreSelect.appendChild(option);
+//     })
+
+    
+//   } catch (error) {
+//     console.error('Error loading genres',error)
+//   }
+// }
+
+// const loadCountriesByContinent = (country) => {
+
+//   const a = ''
+
+// }
+
+
+
+
+
+
+// // We capture elements from the DOM
+// const formData = document.getElementById('formData');
+// const searchInput = document.getElementById('searchInput');
+// const sectionMovies = document.getElementById('containerMovies');
+// const genreSelect = document.getElementById('movieGenre');
+
+
+// const searchAndShowTheMovie = async (e) => {
+//   e.preventDefault();
+
+//   containerMovies.innerHTML = '';
+//   const movieSearch = searchInput.value.trim();
+//   const selectedGenre = genreSelect.value;
+//   console.log('Selected genre:', selectedGenre);
+
+//   try {
+//     let response;
+
+//     if (movieSearch) {
+//       // Search by title
+//       response = await axios.get('/search/movie', {
+//         params: {
+//           query: movieSearch,
+//           language: 'en-US'
+//         }
+//       });
+//     } else if (selectedGenre !== "") {
+//       // Filter by genre
+//       response = await axios.get('/discover/movie', {
+//         params: {
+//           with_genres: selectedGenre,
+//           language: 'en-US'
+//         }
+//       });
+//     }
+
+//     const movies = response.data.results;
+//     renderMovies(movies);
+
+//   } catch (error) {
+//     console.error('Error fetching movies:', error);
+//   }
+
+//   searchInput.value = '';
+// };
+
+// // Function to render all the movies of the search
+// // 1. we pass the searched data array as an argument.
+// // 2. we iterate the array and we call to createMovieCard() function.
+// const renderMovies = (movies) => {
+//   movies.forEach(movie => {    
+//     createMovieCard(movie)    
+//   });
+// }
+
+// const createMovieCard = (movie) => {
+//   const posterUrl = movie.poster_path 
+//     ? `https://image.tmdb.org/t/p/w300${movie.poster_path}`
+//     : 'placeholder.jpg'; // Fallback if no poster
+
+//   // DOM content creation (HTML)
+//   const movieCard = document.createElement('div');
+  
+//   const moviePoster = document.createElement('img');
+//   moviePoster.setAttribute('src', posterUrl);
+//   moviePoster.setAttribute('alt', `${movie.title} poster`);
+
+//   const movieTitle = document.createElement('h1');
+//   movieTitle.textContent = `${movie.title}`;
+
+//   movieCard.append(moviePoster, movieTitle);
+
+//   sectionMovies.appendChild(movieCard);
+// }
+
+// const loadGenres = async () => {
+//   try {
+//     // First get all genre IDs
+//     const response = await axios.get(`/genre/movie/list`, {
+//       params: {
+//         language: 'en-US'
+//       }
+//     })
+
+//     const genres = response.data.genres;
+//     console.log('Genres loaded:', genres);
+
+//     genres.forEach(genre => {
+//       const option = document.createElement('option');
+//       option.value = genre.id;
+//       option.textContent = genre.name;
+//       genreSelect.appendChild(option);
+//     })
+
+    
+//   } catch (error) {
+//     console.error('Error loading genres',error)
+//   }
+// }
+
+// loadGenres()
+
+
+// formData.addEventListener('submit', searchAndShowTheMovie);
+// genreSelect.addEventListener('change', searchAndShowTheMovie);
